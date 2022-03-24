76a406d3aa9026e1431906cafcbf86053153872a
student <student@PC-634.urel.feec.vutbr.cz>
2022-03-24 14:51:15 +0100

    hotove

--- labs/07-display_driver/display_driver/display_driver.srcs/sources_1/new/top.vhd
+++ labs/07-display_driver/display_driver/display_driver.srcs/sources_1/new/top.vhd
@@ -59,15 +59,39 @@ begin
       port map(
           clk        => CLK100MHZ,
           reset      => BTNC,
+          
           data0_i(3) => SW(3),
           data0_i(2) => SW(2),
           data0_i(1) => SW(1),
           data0_i(0) => SW(0),
           -- WRITE YOUR CODE HERE
+          data1_i(3) => SW(7),
+          data1_i(2) => SW(6),
+          data1_i(1) => SW(5),
+          data1_i(0) => SW(4),
           
+          data2_i(3) => SW(11),
+          data2_i(2) => SW(10),
+          data2_i(1) => SW(9),
+          data2_i(0) => SW(8),
+          
+          data3_i(3) => SW(15),
+          data3_i(2) => SW(14),
+          data3_i(1) => SW(13),
+          data3_i(0) => SW(12),
           
           dp_i => "0111",
-          -- WRITE YOUR CODE HERE
+          
+          seg_o(6) => CA,
+          seg_o(5) => CB,
+          seg_o(4) => CC,
+          seg_o(3) => CD,
+          seg_o(2) => CE,
+          seg_o(1) => CF,
+          seg_o(0) => CG,
+          
+          dp_o => DP,
+          dig_o => AN(3 downto 0)
       );
 
   -- Disconnect the top four digits of the 7-segment display
